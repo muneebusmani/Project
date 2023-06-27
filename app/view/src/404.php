a5f7d7b7c5a684b326c253ff5ecbcee8d5261bcc
MuneebUsmani <muneebusmani8355@gmail.com>
2023-06-27 13:01:04 +0500

    experimentation

---
+++ app/view/src/404.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 - Page Not Found</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            text-align: center;
+        }
+
+        .container {
+            max-width: 400px;
+        }
+
+        h1 {
+            font-size: 36px;
+            color: #333333;
+            margin-top: 0;
+        }
+
+        p {
+            font-size: 18px;
+            color: #666666;
+        }
+
+        a {
+            color: #337ab7;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Oops! Page Not Found</h1>
+        <p>The page you're looking for doesn't exist.</p>
+        <p>Go back to <a href="/">Home</a>.</p>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,49 +1,12 @@
 <?php
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = 'app/view/src/';
 
-// Define routes and their corresponding handlers
-$routes = [
-    '/' => 'home',
-    '/about' => 'about',
-    '/contact' => 'contact',
-];
-
-// Get the current URL path
-$path = $_SERVER['REQUEST_URI'];
-
-// Remove query string from the URL if present
-if (($pos = strpos($path, '?')) !== false) {
-    $path = substr($path, 0, $pos);
-}
-
-// Remove leading and trailing slashes from the URL
-$path = trim($path, '/');
-
-// Find the appropriate handler based on the requested route
-if (isset($routes[$path])) {
-    $handler = $routes[$path];
-    // Call the corresponding handler function or include the file
-    if (is_callable($handler)) {
-        call_user_func($handler);
-    } else {
-        include($handler . '.php');
-    }
+// Check if the requested file exists
+if (file_exists($viewDir . $request . '.php')) {
+    require $viewDir . $request . '.php';
 } else {
-    // Handle 404 - Page not found
-    echo '404 - Page not found';
+    // If the file doesn't exist, display the 404 error page
+    require $viewDir . '404.php';
 }
-
-// Handler functions for each route
-function home()
-{
-    echo 'This is the home page.';
-}
-
-function about()
-{
-    echo 'This is the about page.';
-}
-
-function contact()
-{
-    echo 'This is the contact page.';
-}
+?>
