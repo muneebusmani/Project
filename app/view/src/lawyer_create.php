3cb9b3f9e23555ecba994e5a6bdd790d54cd540f
MuneebUsmani <muneebusmani8355@gmail.com>
2023-07-02 15:53:07 +0500

    Lawyer Registration complete

--- app/view/src/lawyer_create.php
+++ app/view/src/lawyer_create.php
@@ -1,9 +1,8 @@
 <?php
+$name=$number=$email=$address=$speciality=$education=$experience=$password= '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['submit'])) {
-        $name=$number=$email=$address=$speciality=$education=$experience=$password= '';
         $errors=[]; 
-
         fetch_post();
         $err_msg=handle_err();
         if($err_msg){
@@ -44,31 +43,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <div class="form-group">
         <label for="Fullname">Full Name</label>
-        <input value="<?php echo $name?>" name="name" type="text" class="form-control" id="Fullname" aria-describedby="emailHelp">    
+        <input value="<?php echo $name; ?>" name="name" type="text" class="form-control" id="Fullname" aria-describedby="emailHelp">    
 
         <label for="phone">Phone Number</label>
-        <input value="<?php echo $number?>" name="number"  type="text" class="form-control" id="phone" aria-describedby="emailHelp">
+        <input value="<?php echo $number; ?>" name="number"  type="text" class="form-control" id="phone" aria-describedby="emailHelp">
 
         <label for="Email">Email address</label>
-        <input value="<?php echo $email?>" name="email"  type="email" class="form-control" id="Email" aria-describedby="emailHelp">
+        <input value="<?php echo $email; ?>" name="email"  type="email" class="form-control" id="Email" aria-describedby="emailHelp">
 
 
         <label for="address">Residential Address</label>
-        <input value="<?php echo $address?>" name="address"  type="text" class="form-control" id="address" aria-describedby="emailHelp">
+        <input value="<?php echo $address; ?>" name="address"  type="text" class="form-control" id="address" aria-describedby="emailHelp">
     </div>
     <div class="form-group">
         <label for="area">Practice Area</label>
-        <input value="<?php echo $speciality?>" name="speciality"  type="text" class="form-control" id="area" aria-describedby="emailHelp">
+        <input value="<?php echo $speciality; ?>" name="speciality"  type="text" class="form-control" id="area" aria-describedby="emailHelp">
 
         <label for="education">Education</label>
-        <input value="<?php echo $education?>" name="education"  type="text" class="form-control" id="education" aria-describedby="emailHelp">
+        <input value="<?php echo $education; ?>" name="education"  type="text" class="form-control" id="education" aria-describedby="emailHelp">
 
         <label for="exp">Experience</label>
-        <input value="<?php echo $experience?>" name="experience"  type="text" class="form-control" id="exp" aria-describedby="emailHelp">
+        <input value="<?php echo $experience; ?>" name="experience"  type="text" class="form-control" id="exp" aria-describedby="emailHelp">
     </div>
     <div class="form-group mb-4">
         <label for="InputPassword1">Password</label>
-        <input value="<?php echo $password?>" name="password"  type="password" class="form-control" id="password">
+        <input value="<?php echo $password; ?>" name="password"  type="password" class="form-control" id="password">
     </div>
 
   <div class="form-group form-check">
